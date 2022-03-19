4b2e227565a4fe39fd9e0ca63c004e0e88421b43
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-19 09:28:29 +0530

    multiply

---
+++ src/multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 20 ;
+        int mul ;
+        mul = a*b ;
+        System.out.println("total sum " +mul);
+
+
+    }
+}
