b592d9993920f2881ef16c8072f29d6f5826c9b3
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-19 09:23:55 +0530

    Subtract

---
+++ src/com/bridgelabz/subtract.java
@@ -0,0 +1,15 @@
+
+    package com.bridgelabz;
+
+    public class subtract {
+        public static void main(String[] args) {
+            int a = 10 ;
+            int b = 20 ;
+            int sub ;
+            sub = a - b ;
+            System.out.println("total sum " +sub);
+
+
+        }
+    }
+
