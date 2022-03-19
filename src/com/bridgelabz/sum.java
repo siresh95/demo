a254a7661eaca8f60042ec0c41e3c573138256f3
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-19 08:54:10 +0530

    Sum

---
+++ src/com/bridgelabz/sum.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 20 ;
+        int sum ;
+        sum = a + b ;
+        System.out.println("total sum " +sum);
+
+
+    }
+}
